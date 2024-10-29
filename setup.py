53414964a3dbe1f0f0d2b875d4032d8e1aedee90
VINOD KUMAR <168188878+vinodkumartz@users.noreply.github.com>
2024-10-29 12:33:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="multilingual assistant",
     version="0.0.1",
-    author="sunny",
-    author_email="sunny.savita@ineuron.ai",
+    author="vinod",
+    author_email="kumar143tz@gmail.com",
     packages=find_packages(),
     install_requires=["SpeechRecognition","pipwin","pyaudio","gTTS","google-generativeai","python-dotenv","streamlit"]
 )
